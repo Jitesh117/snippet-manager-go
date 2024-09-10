b4047588d84ccff3953707807553a8c79bdcf410
Jitesh117 <sahookumarjitesh@gmail.com>
2024-09-10 17:29:24 +0530

    fix: marshalled the password field

--- models/snippet.go
+++ models/snippet.go
@@ -32,7 +32,7 @@ type User struct {
 	ID        uuid.UUID `json:"id"`
 	Username  string    `json:"username"`
 	Email     string    `json:"email"`
-	Password  string    `json:"-"` // cuz it's a password, duh
+	Password  string    `json:"password"`
 	CreatedAt time.Time `json:"created_at"`
 	UpdatedAt time.Time `json:"updated_at"`
 }
