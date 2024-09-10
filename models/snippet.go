eac80d74fa690fe2c9292cba7a65a8a5455b767c
Jitesh117 <sahookumarjitesh@gmail.com>
2024-09-10 18:18:57 +0530

    fix: marshalled the password field, omitempty

--- models/snippet.go
+++ models/snippet.go
@@ -32,7 +32,7 @@ type User struct {
 	ID        uuid.UUID `json:"id"`
 	Username  string    `json:"username"`
 	Email     string    `json:"email"`
-	Password  string    `json:"password"`
+	Password  string    `json:"password,omitempty"`
 	CreatedAt time.Time `json:"created_at"`
 	UpdatedAt time.Time `json:"updated_at"`
 }
